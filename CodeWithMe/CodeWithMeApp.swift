e6d587d7803f58417a478364ed1c3bd1448418c9
Akhilesh Mishra <akhidivs@gmail.com>
2024-10-15 15:17:56 +0530

    Initial Commit

---
+++ CodeWithMe/CodeWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeWithMeApp.swift
+//  CodeWithMe
+//
+//  Created by Akhilesh Mishra on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
